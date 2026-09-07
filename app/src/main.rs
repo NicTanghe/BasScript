6a1a4430c2db043f9c6d365171bcabcb4c5db1c2
Nicolaas Tanghe <nicolaas.tanghe@gmail.com>
2026-09-07 20:50:47 +0200

    fix(app): remove WinitSettings to prevent unrendered window on startup

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,5 +1,3 @@
-use core::time::Duration;
-
 use basscript_ui::UiPlugin;
 #[cfg(target_os = "windows")]
 use bevy::render::{
@@ -13,7 +11,6 @@ use bevy::{
     log::LogPlugin,
     prelude::*,
     window::{WindowPlugin, WindowResizeConstraints},
-    winit::{UpdateMode, WinitSettings},
 };
 
 const MIN_WINDOW_WIDTH: f32 = 640.0;
@@ -76,10 +73,6 @@ fn main() {
     });
 
     App::new()
-        .insert_resource(WinitSettings {
-            focused_mode: UpdateMode::reactive(Duration::from_millis(33)),
-            unfocused_mode: UpdateMode::reactive_low_power(Duration::from_millis(100)),
-        })
         .insert_resource(ClearColor(if NATIVE_TRANSPARENT_WINDOW {
             Color::NONE
         } else {
